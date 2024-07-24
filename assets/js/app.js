7c1e67332ea7a7457e54cef2e1e76416a0ed1ada
jochouca <jonathanloyer1@gmail.com>
2024-07-24 22:03:37 +0200

    row

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,15 +25,5 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     });
 
-    function initMap() {
-        let location = {lat: 48.8566, lng: 2.3522};
-        let map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 14,
-            center: location
-        });
-        let marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
-    }
+  
 });
