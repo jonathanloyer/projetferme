209b0048d02cde74dbf6b2d0229f324981bbe24d
jochouca <jonathanloyer1@gmail.com>
2024-07-24 11:22:39 +0200

    main test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+function initMap() {
+    var location = {lat: 48.8566, lng: 2.3522}; // Coordonées de l'emplacement
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
     // let elems = document.querySelectorAll('.sidenav');
     // let instances = M.Sidenav.init(elems);
 });
